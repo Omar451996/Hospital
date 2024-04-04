6d2f469c3b1a6de3c5b71ca3ba2a655f2523a82c
Omar451996 <omarhelmy345@gmail.com>
2024-04-04 13:11:55 +0200

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserTableSeeder::class,
+            AdminTableSeeder::class,
+            AppointmentSeeder::class,
+            SectionTableSeeder::class,
+            DoctorTableSeeder::class,
+            ImageTableSeeder::class,
+        ]);
+    }
+}
